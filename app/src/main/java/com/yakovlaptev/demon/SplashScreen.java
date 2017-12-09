889a34b6d48a25b164b1f6b0a419ee285d556bdf
Yakov <yakovlaptev@yandex.ru>
2017-12-09 20:56:45 +0300

    Splash screen

---
+++ app/src/main/java/com/yakovlaptev/demon/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.yakovlaptev.demon;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1800);
+    }
+
+}
